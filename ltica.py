54828b14b9d4501648bef3ebd7c3f60dda1088a5
seita <seita.1224.ds@outlook.com>
2019-08-11 02:25:25 +0900

    設定ファイル読み込み用クラス作成 今後設定環境などはこのクラスから読み込ませる

--- ltica.py
+++ ltica.py
@@ -1,7 +1,23 @@
-import gpiozero
 import time
+import yaml
 
-let = gpiozero.LED(4)
-let.on()
-time.sleep(3)
-let.off()
+
+class ConfigParse:
+    '''
+    このクラスは設定ファイルから設定を読み込むファイルです。
+    '''
+    HOST_CONFIG = None
+    HOST_TYPE = None
+
+    def __init__(self):
+        self.parse_config()
+
+    def parse_config(self):
+        config_file = open('config.yaml', 'r')
+        config_txt = config_file.read()
+        parse_config = yaml.safe_load(config_txt)
+        self.HOST_CONFIG = parse_config.get('host_config')
+        self.HOST_TYPE = self.HOST_CONFIG.get('host_type')
+
+
+print(ConfigParse().HOST_TYPE)
