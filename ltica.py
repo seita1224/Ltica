d54369d9d64981d592cdfb8cfca579134e1a8614
seita1224 <seita.1224.ds@outlook.com>
2019-10-11 13:52:48 +0900

    Update ltica.py

--- ltica.py
+++ ltica.py
@@ -15,3 +15,16 @@ set_pixel(0, 255, 0, 0)
 show()
 
 time.sleep(1)
+
+
+#ここから新ソース
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(25, GPIO.OUT)
+
+GPIO.output(25, GPIO.HIGH)
+time.sleep(2) # この間は点灯し続ける
+
+GPIO.cleanup() # <- 消灯
