a7cb878168aa15a753f9b08c631b1f639cd17fd5
seita <seita.1224.ds@outlook.com>
2019-10-12 14:02:21 +0900

    Lチカ用ソースに入れ替え

--- ltica.py
+++ ltica.py
@@ -1,23 +1,3 @@
-import time
-
-compile_lib = ''
-try:
-    from blinkt import set_pixel, set_brightness, show, clear
-    compile_lib = 'blinkt'
-except ImportError:
-    from compile.blinkt import set_pixel, set_brightness, show, clear
-    compile_lib = 'compile.blinkt'
-
-set_brightness(0.5)
-
-clear()
-set_pixel(0, 255, 0, 0)
-show()
-
-time.sleep(1)
-
-
-#ここから新ソース
 import RPi.GPIO as GPIO
 import time
 
