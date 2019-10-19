512de686669fe3102378e875bbd4cdff18dc9897
seita <seita.1224.ds@outlook.com>
2019-10-19 11:48:51 +0900

    出力GPIOの修正

--- ltica.py
+++ ltica.py
@@ -4,10 +4,10 @@ import time
 for i in range(10):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(2, GPIO.OUT)
-    time.sleep(1)  # この間は点灯し続ける
     GPIO.output(2, GPIO.HIGH)
     time.sleep(1) # この間は点灯し続ける
     GPIO.cleanup()  # <- 消灯
+    time.sleep(1)  # この間は点灯し続ける
 
 
 
